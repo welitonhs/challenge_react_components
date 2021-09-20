ee00dfea6cc56d1135259c27563c7337be660e3a
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2021-09-20 14:57:01 -0300

    Using components with parents props

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+
+import { useState } from 'react';
+import { Button } from './Button';
+interface Genre {
+  id: number;
+  title: string;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+}
+
+interface SideBarProps {
+  selectedGenre: number;
+  genres: Genre[];
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {props.genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => props.handleClickButton(genre.id)}
+          selected={props.selectedGenre === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
