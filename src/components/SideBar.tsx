47a30b7523abe8f7661b1ccca4ac4a3b7786c12d
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2021-09-20 15:41:38 -0300

    React components with context API

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { useContext } from 'react';
+
+import { Button } from './Button';
+import { MoviesContext } from '../contexts/MoviesContext';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, setSelectedGenreId } = useContext(MoviesContext);
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
